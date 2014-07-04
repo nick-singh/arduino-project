a154e6145276af9bd28948be3c23ba41a1e0dacf
wryi <cherlton.millette@gmail.com>
2014-07-03 22:18:12 -0400

    Updates to the gps script programs

--- demo/demo.ino
+++ demo/demo.ino
@@ -8,12 +8,11 @@
 */
 
 TinyGPS gps;
-SoftwareSerial ss(2, 3);
+//SoftwareSerial ss(2, 3);
 
 void setup()
 {
-  Serial.begin(115200);
-  ss.begin(4800);
+  Serial.begin(38400);
   
   Serial.print("Simple TinyGPS library v. "); Serial.println(TinyGPS::library_version());
   Serial.println("by Mikal Hart");
@@ -29,29 +28,33 @@ void loop()
   // For one second we parse GPS data and report some key values
   for (unsigned long start = millis(); millis() - start < 1000;)
   {
-    while (ss.available())
+    while (Serial.available())
     {
-      char c = ss.read();
-      // Serial.write(c); // uncomment this line if you want to see the GPS data flowing
+      char c = Serial.read();
+      //Serial.write(c); // uncomment this line if you want to see the GPS data flowing
       if (gps.encode(c)) // Did a new valid sentence come in?
         newData = true;
     }
   }
 
-  if (newData)
-  {
+ // if (newData)
+ // {
     float flat, flon;
     unsigned long age;
     gps.f_get_position(&flat, &flon, &age);
     Serial.print("LAT=");
     Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
+    Serial.println();
     Serial.print(" LON=");
     Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
+    Serial.println();
     Serial.print(" SAT=");
     Serial.print(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
+    Serial.println();
     Serial.print(" PREC=");
     Serial.print(gps.hdop() == TinyGPS::GPS_INVALID_HDOP ? 0 : gps.hdop());
-  }
+    Serial.println();
+  //}
   
   gps.stats(&chars, &sentences, &failed);
   Serial.print(" CHARS=");
